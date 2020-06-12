4660d4aace66243dffa67b4bb32df48cadb4c608
Carey <careycbaldwin@gmail.com>
2020-06-12 07:54:39 -0400

    added modals for relevant applications of b-hound technology

--- c-modal.js
+++ c-modal.js
@@ -1,25 +1,25 @@
 // Get the modal
-var modal = document.getElementById("summaryModal");
+var cModal = document.getElementById("covidModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("summaryBtn");
+var cBtn = document.getElementById("covidBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[3];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+cBtn.onclick = function() {
+  cModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  cModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == cModal) {
+    cModal.style.display = "none";
   }
 }
--- p-modal.js
+++ p-modal.js
@@ -1,25 +1,25 @@
 // Get the modal
-var modal = document.getElementById("summaryModal");
+var pModal = document.getElementById("policeModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("summaryBtn");
+var pBtn = document.getElementById("policeBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[3];
+var span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+pBtn.onclick = function() {
+  pModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  pModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == pModal) {
+    pModal.style.display = "none";
   }
 }
